491e53bb937e4b43a33d229744b4fddedca7581c
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-07-16 19:10:26 +0300

    added gitignore

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 app.get('/signout', (req, res) => {
+  // вот здесь кука удаляется, у меня всё работает и удаляется
   res.clearCookie('jwt', { httpOnly: true });
   res.status(200).json({ message: 'кука удалена' });
 });
